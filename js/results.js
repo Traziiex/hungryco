d930df81de616acd96f13f7146b8d3289cfcfd4e
Noah <noah.blanchard@etu.univ-lyon1.fr>
2021-03-11 17:34:38 +0100

    Added modal popup on click

--- js/results.js
+++ js/results.js
@@ -3,10 +3,15 @@ const searchText = query.replaceAll("%20", " ");
 const key = "9a82bba66428019f581ebff5d7635c9c";
 const id = "4b353692";
 
+let activeRecipes;
+
+const recipeContainer = document.getElementById("recipe-container");
+
 const searchBtn = document.getElementById("search-button");
 const searchBar = document.getElementById("search-bar")
 
 let hits;
+let code = "";
 
 const getRecipe = async () => {
     const response = await fetch(`https://api.edamam.com/search?q=${searchText}&app_id=${id}&app_key=${key}`);
@@ -21,11 +26,36 @@ const firstLoad = () => {
 }
 
 const displayResults = () => {
-    hits.map(hit => console.log(hit.recipe.label))
+    hits.map(hit => injectCode(hit))
+    recipeContainer.innerHTML = code;
+    activeRecipes = document.querySelectorAll(".box")
+    activeRecipes.forEach((button) => {
+        button.onclick = () => {
+          if(button.classList.contains("zoomed")){
+              button.classList.remove("zoomed");
+          }else{
+              button.classList.add("zoomed");
+          }
+        }
+      })
 }
 
 const searchMethod = () => {
     window.location.href = `./results.html?${searchBar.value}`;
+}
+
+const injectCode = (hit) => {
+    code += `
+    <div class="box">
+        <div class="recipe">
+            <span class="recipe-label">${hit.recipe.label}</span>
+            <img src="${hit.recipe.image}"></img>
+            <ul>
+                ${hit.recipe.ingredientLines.map(ingredient => `<li>${ingredient}</li>`)}
+            </ul>
+        </div>
+    </div>
+    `;
 }
 
 searchBtn.addEventListener("click", searchMethod);
